a3fda900f2ed07596422053b051bbc022392b392
Kenneth Madsen <mail@kenneth.eu>
2020-03-01 21:20:13 +0100

    just trying

--- src/com/kennetheu/gini/Main.java
+++ src/com/kennetheu/gini/Main.java
@@ -5,9 +5,9 @@ import java.text.DecimalFormat;
 public class Main {
 
     public static void main(String[] args) {
-	    //CreateData income2 = new CreateData(10000);
-        //System.out.println("Printing");
-        //income2.printIncomes();
+	    CreateData income2 = new CreateData(10000);
+        System.out.println("Printing");
+        income2.printIncomes();
         DecimalFormat df=new DecimalFormat("0.000");
         for (int i = 0; i < 10; i++) {
             CreateData income = new CreateData(5_000_000);
