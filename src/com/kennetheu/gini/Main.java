d1dd92a12bebceaf3ba9e6873b7b57a9843dcbe6
Kenneth Madsen <mail@kenneth.eu>
2020-03-05 17:25:36 +0100

    Clean up

--- src/com/kennetheu/gini/Main.java
+++ src/com/kennetheu/gini/Main.java
@@ -1,11 +1,8 @@
 package com.kennetheu.gini;
 
 public class Main {
-
     public static void main(String[] args) {
-
         int[] fordeling = new int[] {40, 50, 60, 20, 30, 40, 30, 50, 40, 20};
-
         CreateData myIncomes = new CreateData(1_000,
                 50_000,
                 2_000_000);
