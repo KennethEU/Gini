030948a4052358070fc569d925c19074ab8d5e89
Kenneth Madsen <mail@kenneth.eu>
2020-03-14 19:20:10 +0100

    Making the menu - missing the taxing system

--- src/com/kennetheu/gini/Main.java
+++ src/com/kennetheu/gini/Main.java
@@ -1,16 +1,91 @@
 package com.kennetheu.gini;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        int[] myDist = new int[] {40, 50, 60, 20, 30, 40, 30, 50, 40, 20};
-        CreateData myIncomes = new CreateData(10,
-                150_000,
-                myDist);
+//        int[] myDist = new int[]{40, 50, 60, 20, 30, 40, 30, 50, 40, 20};
+//        CreateData myIncomes = new CreateData(10,
+//                150_000,
+//                myDist);
+//
+//        int[] myTaxSystem = new int[]{50_000, 30, 400_000, 20, 700_000, 15};
+//
+//        TaxPayers myTaxPayers = new TaxPayers(myIncomes, myTaxSystem);
+//        myTaxPayers.printInfo();
 
-        int[] myTaxSystem = new int[] {50_000,30,400_000,20,700_000,15};
+        char choice;
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Velkommen til Kenneths Gini-program!");
+        System.out.println("------------------------------------");
+        CreateData myIncomes = null;
+        // Create a menu
+        do {
 
-        TaxPayers myTaxPayers = new TaxPayers(myIncomes, myTaxSystem);
-        myTaxPayers.printInfo();
+            System.out.println("Du har nu følgende valgmuligheder:");
+            System.out.println("[1] Lav en indkomstfordeling");
+            System.out.println("[2] Print Gini for indkomstfordeling (før skat)");
+            System.out.println("[3] Lav et skattesystem og beskat din indkomst fordeling");
+            System.out.println("[4] Print Gini for indkomstfordeling (efter skat)");
 
+            System.out.println();
+            System.out.println("[9] Luk programmet");
+            System.out.print("Dit valg: ");
+
+            choice = keyboard.next().charAt(0);
+            System.out.println();
+
+            switch (choice) {
+                case '1':
+                    myIncomes = makeIncome();
+                    break;
+                case '2':
+                    if (myIncomes != null) {
+                        System.out.println("Gini-koefficent for indkomstfordelingen: " + myIncomes.getGiniCoef());
+                        System.out.println();
+                    } else {
+                        System.out.println("FEJL: Du har ikke lavet en indkomstfordeling endnu");
+                        System.out.println();
+                    }
+                    break;
+                case '3':
+                    if (myIncomes != null) {
+                        // lav skattesystem her!!
+                        int[] myTaxSystem = new int[]{50_000, 30, 400_000, 20, 700_000, 15};
+                        TaxPayers myTaxPayers = new TaxPayers(myIncomes, myTaxSystem);
+                        myTaxPayers.printInfo();
+                    } else {
+                        System.out.println("FEJL: Du har ikke lavet en indkomstfordeling endnu");
+                        System.out.println();
+                    }
+                    break;
+
+                case '9':
+                    break;
+                default:
+                    System.out.println("FEJL: Valg ikke gyldigt");
+                    System.out.println();
+            }
+
+        } while (choice != '9');
+        System.out.println("Tak for denne gang!");
     }
+    public static CreateData makeIncome() {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.print("Hvor mange enheder skal fordelingen inddeles i? ");
+        int size = keyboard.nextInt();
+        int[] myDist = new int[size];
+        System.out.println("Du skal nu udfylde antal personer i hver inddeling:");
+        for (int i = 0; i < myDist.length; i++) {
+            System.out.print(i+1 + ": ");
+            myDist[i] = keyboard.nextInt();
+
+        }
+        System.out.print("Hvor stor spredning dækker hver inddeling over? ");
+        int span = keyboard.nextInt();
+        CreateData myIncomes = new CreateData(myDist.length, span, myDist);
+        System.out.println();
+        return myIncomes;
+    }
+
 }
